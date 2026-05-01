2f47de8152775aae3b248e49967128ae93076d78
CaelumNova <Austin.noice@outlook.com>
2026-04-30 23:00:18 -0400

    Update middleware.ts

--- citadel-status/middleware.ts
+++ citadel-status/middleware.ts
@@ -1,10 +1,10 @@
 import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-const isPublicRoute = createRouteMatcher(["/sign-in(.*)", "/sign-up(.*)"]);
+const isPublicRoute = createRouteMatcher(["/sign-in(.*)"]);
 
-export default clerkMiddleware((auth, req) => {
+export default clerkMiddleware(async (auth, req) => {
   if (!isPublicRoute(req)) {
-    auth().protect();
+    await auth.protect();
   }
 });
 
